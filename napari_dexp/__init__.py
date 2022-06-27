785fca3ed8355eb06356b991b3052c888afbd216
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-06-27 11:04:13 -0700

    Compatibility with tools menu and assistant (#12)
    * made first menu entry: butterworth denoising
    
    * Added RL deconvolution
    
    * added sobel filter
    
    * added area_opening
    
    * added area closing, white-top-hat, black-top-hat
    
    * added dehaze
    
    * added lipschitz_continuity_correction
    
    * better function naming
    
    * minor
    
    * fix import statements
    
    * added tests executing all functions
    
    to see if they run
    
    * add cupy to installation instructions
    
    * pin napari version to make menus work

--- napari_dexp/__init__.py
+++ napari_dexp/__init__.py
@@ -5,4 +5,6 @@ except ImportError:
 
 from ._reader import napari_get_reader
 from ._writer import napari_get_writer, napari_write_image, napari_write_labels
+from ._function import napari_experimental_provide_function, denoise_butterworth, lucy_richardson_deconvolution, \
+    sobel_filter, area_opening,area_closing,area_white_top_hat,area_black_top_hat,dehaze,lipschitz_continuity_correction
 
---
+++ napari_dexp/_function.py
@@ -0,0 +1,155 @@
+import napari
+from napari_tools_menu import register_function
+from napari_time_slicer import time_slicer
+from napari_plugin_engine import napari_hook_implementation
+
+
+@napari_hook_implementation
+def napari_experimental_provide_function():
+    return [denoise_butterworth, lucy_richardson_deconvolution, sobel_filter, area_opening,area_closing,
+            area_white_top_hat,area_black_top_hat,dehaze,lipschitz_continuity_correction]
+
+
+@register_function(menu="Filtering / noise removal > Butterworth (dexp)")
+@time_slicer
+def denoise_butterworth(
+    image: napari.types.ImageData,
+    freq_cutoff: float = 0.5,
+    order: float = 1,
+    padding: int = 32
+) -> napari.types.ImageData:
+    from dexp.processing import denoising
+
+    return denoising.denoise_butterworth(image=image,
+                                         freq_cutoff=freq_cutoff,
+                                         order=order,
+                                         padding=padding)
+
+
+@register_function(menu="Filtering / deconvolution > Lucy-Richardson (dexp)")
+@time_slicer
+def lucy_richardson_deconvolution(
+    image: napari.types.ImageData,
+    psf: napari.types.ImageData,
+    num_iterations: int = 10
+) -> napari.types.ImageData:
+    from dexp.processing import deconvolution
+
+    return deconvolution.lucy_richardson_deconvolution(
+        image=image,
+        psf=psf,
+        num_iterations=num_iterations
+    )
+
+@register_function(menu="Filtering / edge enhancement > Sobel (dexp)")
+@time_slicer
+def sobel_filter(
+    image: napari.types.ImageData,
+    exponent: int = 2,
+    gamma: float = 1
+) -> napari.types.ImageData:
+
+    from dexp.processing.filters import sobel_filter
+
+    return sobel_filter.sobel_filter(
+        image=image,
+        exponent=exponent,
+        gamma=gamma)
+
+
+@register_function(menu="Filtering > Area opening (dexp)")
+@time_slicer
+def area_opening(
+    image: napari.types.ImageData,
+    area_threshold: float = 100,
+    sampling: int = 1,
+) -> napari.types.ImageData:
+
+    from dexp.processing import morphology
+
+    return morphology.area_opening(image=image, area_threshold=area_threshold, sampling=sampling)
+
+
+@register_function(menu="Filtering > Area closing (dexp)")
+@time_slicer
+def area_closing(
+    image: napari.types.ImageData,
+    area_threshold: float = 100,
+    sampling: int = 1,
+) -> napari.types.ImageData:
+
+    from dexp.processing import morphology
+
+    return morphology.area_closing(image=image, area_threshold=area_threshold, sampling=sampling)
+
+
+@register_function(menu="Filtering > Area white top hat (dexp)")
+@time_slicer
+def area_white_top_hat(
+    image: napari.types.ImageData,
+    area_threshold: float = 100,
+    sampling: int = 1,
+) -> napari.types.ImageData:
+
+    from dexp.processing import morphology
+
+    return morphology.area_white_top_hat(image=image, area_threshold=area_threshold, sampling=sampling)
+
+
+@register_function(menu="Filtering > Area black top hat (dexp)")
+@time_slicer
+def area_black_top_hat(
+    image: napari.types.ImageData,
+    area_threshold: float = 100,
+    sampling: int = 1,
+) -> napari.types.ImageData:
+
+    from dexp.processing import morphology
+
+    return morphology.area_black_top_hat(image=image, area_threshold=area_threshold, sampling=sampling)
+
+
+@register_function(menu="Filtering > Dehaze (dexp)")
+@time_slicer
+def dehaze(
+    image: napari.types.ImageData,
+    size: int = 21,
+    downscale: int = 4,
+    minimal_zero_level: float = 0,
+    correct_max_level: bool = True
+) -> napari.types.ImageData:
+
+    from dexp.processing.restoration import dehazing
+
+    return dehazing.dehaze(
+        image=image,
+        size=size,
+        downscale=downscale,
+        minimal_zero_level=minimal_zero_level,
+        correct_max_level=correct_max_level,
+        in_place=False
+    )
+
+
+@register_function(menu="Filtering > Lipschitz continuity correction (dexp)")
+@time_slicer
+def lipschitz_continuity_correction(
+    image: napari.types.ImageData,
+    num_iterations: int = 2,
+    correction_percentile: float = 0.1,
+    lipschitz: float = 0.1,
+    max_proportion_corrected: float = 1,
+    decimation: int = 8
+) -> napari.types.ImageData:
+
+    from dexp.processing.restoration import lipshitz_correction
+
+    return lipshitz_correction.lipschitz_continuity_correction(
+        image=image,
+        num_iterations=num_iterations,
+        correction_percentile=correction_percentile,
+        lipschitz=lipschitz,
+        max_proportion_corrected=max_proportion_corrected,
+        decimation=decimation,
+        in_place = False)
+
